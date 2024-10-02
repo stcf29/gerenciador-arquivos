650a1e7f9fe87909dbb9c79fd10d3c5ae7f7d069
Sérgio <sergio.filho@pdcase.com.br>
2024-10-01 22:59:52 -0300

    Update MyComponent.test.js

--- front-rep-arquivos/src/components/MyComponent.test.js
+++ front-rep-arquivos/src/components/MyComponent.test.js
@@ -4,16 +4,16 @@ import '@testing-library/jest-dom'
 
 
 test('mostra uma lista de diretórios', () => {
-  // Simula os diretórios que serão passados como props
+  
   const directories = [
     { id: 1, nome: 'Documentos', files: [{ id: 1, nome: 'file1.txt' }] },
     { id: 2, nome: 'Imagens', files: [{ id: 2, nome: 'image1.jpg' }] },
   ];
 
-  // Renderiza o componente com os diretórios
+  
   render(<MyComponent directories={directories} />);
 
-  // Verifica se os nomes dos diretórios aparecem na tela
+
   expect(screen.getByText('Documentos - 1 arquivos')).toBeInTheDocument();
   expect(screen.getByText('Imagens - 1 arquivos')).toBeInTheDocument();
 });
@@ -21,6 +21,6 @@ test('mostra uma lista de diretórios', () => {
 test('mostra mensagem quando não há diretórios', () => {
   render(<MyComponent directories={[]} />);
 
-  // Verifica se a mensagem de "Nenhum diretório encontrado" aparece
+  
   expect(screen.getByText('Nenhum diretório encontrado')).toBeInTheDocument();
 });
